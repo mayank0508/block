c48d036dd0e92cace0589919ba1ae26936f595eb
mayank0508 <thakurmayank0508@gmail.com>
2021-01-30 03:14:59 +0530

    added type.ts

---
+++ src/app/types.ts
@@ -0,0 +1,14 @@
+export interface Poll{
+  id: number; //12
+  question: string; // Which days of the week youlike most;
+  results: number[]; //[0,0,1,3,5,22,33,112]
+  options: string[]; // ['monday', 'tuesday',..]
+  thumbnail: string; // http://image.png
+}
+
+export interface Voter{
+  id: string; //0xfsjjsfgbjhbgjfgj
+  voted: number[];
+  
+
+}
