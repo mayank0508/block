1bf4de051c9a55cd89aae35d16d60c5d5444bf9a
mayank0508 <thakurmayank0508@gmail.com>
2021-02-01 03:43:33 +0530

    Angular Frame work added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +11,7 @@ import { PollCreateComponent } from './poll-create/poll-create.component';
     PollCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/poll-create/poll-create.component.ts
+++ src/app/poll-create/poll-create.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-poll-create',
   templateUrl: './poll-create.component.html',
   styleUrls: ['./poll-create.component.scss']
 })
-export class PollCreateComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PollCreateComponent {
+pollForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+this.pollForm =this.fb.group({
+  question: this.fb.control('', [Validators.required]),
+  image: this.fb.control('',),
+  option1: this.fb.control('',),
+  option2: this.fb.control('',),
+  option3: this.fb.control('',),
+})
+}
 }
