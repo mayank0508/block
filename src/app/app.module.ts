e4f22ce845e5d420c57d927d728905d6ba83a6cd
mayank0508 <thakurmayank0508@gmail.com>
2021-02-01 01:15:25 +0530

    poll added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PollCreateComponent } from './poll-create/poll-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PollCreateComponent
   ],
   imports: [
     BrowserModule
