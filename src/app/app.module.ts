4f7afd834a9c54fc33bdba827d7ed53f563ea25c
mayank0508 <thakurmayank0508@gmail.com>
2021-02-11 01:46:14 +0530

    pollVote component logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PollCreateComponent } from './poll-create/poll-create.component';
 import { PollComponent } from './poll/poll.component';
+import { PollVoteComponent } from './poll-vote/poll-vote.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PollCreateComponent,
-    PollComponent
+    PollComponent,
+    PollVoteComponent
   ],
   imports: [
     BrowserModule, FormsModule, ReactiveFormsModule
---
+++ src/app/poll-vote/poll-vote.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-poll-vote',
+  templateUrl: './poll-vote.component.html',
+  styleUrls: ['./poll-vote.component.scss']
+})
+export class PollVoteComponent implements OnInit {
+
+  options = ['MOnday', 'Tuesday', 'Wedneaday'];
+  voteForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+     this.voteForm = this.fb.group({
+       selected: this.fb.control("", [Validators.required])
+     });
+   }
+
+
+  ngOnInit(): void {}
+
+  submitForm(){
+    console.log(this.voteForm.value);
+  }
+}
